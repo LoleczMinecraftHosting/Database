1356b8b6e294b24090b34450c171099160c08560
loleczkowo <szymon.kondratek@gmail.com>
2026-05-24 05:56:30 +0200

    add edit status

--- api/handlers/servers.py
+++ api/handlers/servers.py
@@ -4,6 +4,7 @@ from core.database import (
     edit_server_display_name, edit_server_host,
     edit_server_node, edit_server_ram, edit_server_start_command, edit_server_stop_command,
     edit_server_working_directory,
+    update_server_status,
 )
 from api.api_core import APIHandler
 from api.utils import APIReturn, read_dict, api_get_json
@@ -98,6 +99,17 @@ def post_edit_server_ram(headers, query, data, server_name):
     if success is not True:
         return success
     result = edit_server_ram(name=server_name, ram_min_mb=min_ram_mb, ram_max_mb=max_ram_mb)
+    if result.status == DBStatus.INVALID_INPUT:
+        return APIReturn({"error": "invalid input"}, code=400)
+    if result.status == DBStatus.NOT_FOUND:
+        return APIReturn({"error": "server does not exist"}, code=404)
+    return APIReturn({"status": "ok"})
+
+
+@APIHandler.post("/server/{server_name}/status")
+def post_update_server_status(headers, query, data, server_name):
+    new_value = api_get_json(headers, data)
+    result = update_server_status(name=server_name, status=new_value)
     if result.status == DBStatus.INVALID_INPUT:
         return APIReturn({"error": "invalid input"}, code=400)
     if result.status == DBStatus.NOT_FOUND:
