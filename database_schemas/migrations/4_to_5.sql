ec3977076e198fb837b6c92d59e8f137dcedb7c6
loleczkowo <loleczkowo@loleczkowo.com>
2026-05-31 21:59:26 +0200

    whoops

--- database_schemas/migrations/4_to_5.sql
+++ database_schemas/migrations/4_to_5.sql
@@ -6,12 +6,12 @@ PRAGMA foreign_keys = OFF;
 CREATE TABLE permissions_new (
     subject_type TEXT NOT NULL,
     subject_id TEXT NOT NULL,
-    server_name TEXT NOT NULL,
+    server_name TEXT,
     perms INTEGER NOT NULL,
 
     CHECK (subject_type IN ('default', 'user', 'guild', 'role')),
 
-    PRIMARY KEY (
+    UNIQUE (
         subject_type,
         subject_id,
         server_name
